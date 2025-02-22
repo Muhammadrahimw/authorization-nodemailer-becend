da1b5d9c3415a2ddf0eee4f18f6d24676c577b8d
Muhammadrahimw <muqalla21@gmail.com>
2025-02-22 15:40:15 +0500

    fixing

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -6,7 +6,7 @@ dotenv.config();
 export const verifyToken = (req, res, next) => {
 	try {
 		const token = req.headers.authorization?.split(" ")[1];
-		if (!token) throw new CustomError(401, `Access denied. No token provided`);
+		if (!token) throw new CustomError(200, `Access denied. No token provided`);
 		const decoded = jwt.verify(token, process.env.SECRET_KEY);
 		req.user = decoded;
 		next();
--- utils/jwt.js
+++ utils/jwt.js
@@ -3,5 +3,5 @@ import jwt from "jsonwebtoken";
 dotenv.config();
 
 export const signInJwt = (params) => {
-	return jwt.sign(params, process.env.SECRET_KEY, {expiresIn: "30m"});
+	return jwt.sign(params, process.env.SECRET_KEY, {expiresIn: "60m"});
 };
